1c76f0988160f946adb486282ffbbf03002c93c1
minecraftminegun <159155336+minecraftminegun@users.noreply.github.com>
2024-06-30 21:29:35 +0200

    Create .lua

---
+++ .lua
@@ -0,0 +1,5 @@
+if game.PlaceId == 8888615802 then
+loadstring(game:HttpGet("https://raw.githubusercontent.com/RobloxHackingProject/CHHub/main/games/rainbow%20friends%20chapter%201.lua"))()
+elseif game.PlaceId == 13622981808 then
+loadstring(game:HttpGet("https://raw.githubusercontent.com/RobloxHackingProject/CHHub/main/games/rainbow%20friends%20chapter%202.lua"))()
+end
